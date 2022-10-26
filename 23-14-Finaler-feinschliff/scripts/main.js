9abd12f95b62334695d65fd3c13c044893039ab1
raniaw@web.de <raniaw@web.de>
2022-10-26 20:06:16 +0200

    Add Finaler-feinschliff

---
+++ 23-14-Finaler-feinschliff/scripts/main.js
@@ -0,0 +1,64 @@
+function checkScroll() {
+    const videos = document.querySelectorAll(".story__video");
+
+    for(const video of videos) {
+        const boundingRect = video.getBoundingClientRect();
+        const visible = (boundingRect.top + boundingRect.height > 0 &&
+            boundingRect.top < window.innerHeight);
+
+        
+        if (visible) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+}
+
+checkScroll();
+window.addEventListener('load', checkScroll, false);
+window.addEventListener('scroll', checkScroll, false);
+window.addEventListener('resize', checkScroll, false);
+
+
+function addMousePositionToCss() {
+    const elements = document.querySelectorAll(".offers__card-container");
+    for(const element of elements) {
+        element.addEventListener("mousemove", function(e) {
+            var rect = element.getBoundingClientRect();
+            var x = e.clientX - rect.left; 
+            var y = e.clientY - rect.top;
+            element.style = "--mouse-x:" + (x / element.offsetWidth) + ";--mouse-y:" + (y / element.offsetHeight) + ";";
+        });
+        element.addEventListener("mouseleave", function(e) {
+            element.style = "";
+        });
+    }
+}
+
+if (document.readyState === "complete" || document.readyState === "interactive") {
+    setTimeout(addMousePositionToCss, 1);
+} else {
+    document.addEventListener("DOMContentLoaded", addMousePositionToCss, false);
+}
+
+
+
+function bookingModalFixClose() {
+    const elements = document.querySelectorAll(".booking-modal__close");
+    for(const element of elements) {
+        element.addEventListener("click", function() {
+            const scrollY = window.scrollY;
+            const scrollX = window.scrollX;
+            setTimeout(function() {
+                window.scrollTo({top: scrollY, left: scrollX});
+            }, 1);
+        });
+    }
+}
+
+if (document.readyState === "complete" || document.readyState === "interactive") {
+    setTimeout(bookingModalFixClose, 1);
+} else {
+    document.addEventListener("DOMContentLoaded", bookingModalFixClose, false);
+}
